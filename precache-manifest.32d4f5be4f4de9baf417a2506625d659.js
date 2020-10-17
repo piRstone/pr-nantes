e8534f9b27a8508c236c461cc19a1f960ee5fb27
piRstone <pierre@lavalley.fr>
2020-10-17 22:00:53 +0200

    Updates

---
+++ precache-manifest.32d4f5be4f4de9baf417a2506625d659.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df0e1097b632220abd68aee9f9abe4e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4c2ae77cacd606ee1f6",
+    "url": "/static/css/main.b7f39e75.chunk.css"
+  },
+  {
+    "revision": "a2cd65595d270fd796a7",
+    "url": "/static/js/2.cbb1006c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cbb1006c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4c2ae77cacd606ee1f6",
+    "url": "/static/js/main.16d74b50.chunk.js"
+  },
+  {
+    "revision": "62466ec469cd7c0d7cc1",
+    "url": "/static/js/runtime-main.ce2016e4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.375b3479a5639d4dff9a547ce3e0da81.js"
+  "/precache-manifest.32d4f5be4f4de9baf417a2506625d659.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
