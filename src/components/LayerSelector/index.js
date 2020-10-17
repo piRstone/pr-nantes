3cc2e49038a23dadd5bca1e69a638e231713d4b7
piRstone <pierre@lavalley.fr>
2020-10-17 14:58:06 +0200

    Handle click outsite layers popup

--- src/components/LayerSelector/index.js
+++ src/components/LayerSelector/index.js
@@ -1,29 +1,34 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLayerGroup } from "@fortawesome/free-solid-svg-icons";
 import "./style.css";
 
+import { useOnClickOutside } from '../../utils/hooks';
 import { AppDataContext } from "../../dataProvider";
 
 const LayerSelector = ({ onToggleAllParks }) => {
   const [showPopup, setShowPopup] = useState(false);
   const { appData } = useContext(AppDataContext);
 
+  const popupRef = useRef();
+  useOnClickOutside(popupRef, () => { setShowPopup(false) });
+
   return (
-    <div className="layer-selector">
+    <div ref={popupRef} className="layer-selector">
       <div className="layer-button" onClick={() => setShowPopup(!showPopup)}>
         <FontAwesomeIcon icon={faLayerGroup} className="layer-icon" size="lg" />
       </div>
       {showPopup && (
         <div className="layer-popup">
-          <div>
+          <div className="layer-row">
             <input
               id="toggle-all-parks"
+              className="layer-input"
               type="checkbox"
               checked={appData.showAllParks}
               onChange={onToggleAllParks}
             />
-            <label htmlFor="toggle-all-parks">
+            <label htmlFor="toggle-all-parks" className="layer-label">
               Afficher tous les parkings relais
             </label>
           </div>
